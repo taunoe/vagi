f45c2308c2987d9c15f1a96c6d7f7106dbe2af1f
Tauno Erik <sedumacre@gmail.com>
2023-05-19 22:04:32 +0300

    py readme

---
+++ python/README.md
@@ -0,0 +1,36 @@
+# Python
+
+## Create Virtual Environment
+
+Set-Up
+
+```Bash
+pip install virtualenv
+python3 --version
+python3.10 -m venv env
+source env/bin/activate
+```
+
+Deactivate Virtual Environment
+
+```Bash
+~ deactivate
+```
+
+Save installed libraries to file
+
+```Bash
+pip list
+pip freeze > requirements.txt
+pip install -r requirements.txt
+```
+
+Install libraries
+
+```Bash
+pip install ttkbootstrap
+```
+
+## Learn
+
+- [The ultimate introduction to modern GUIs in Python [ with tkinter ]](https://www.youtube.com/watch?v=mop6g-c5HEY)
--- python/main.py
+++ python/main.py
@@ -3,53 +3,50 @@
 File:    main.py
 Author:  Tauno Erik
 Started: 18.05.2023
-Edited:  18.05.2023
+Edited:  19.05.2023
+
+pip install ttkbootstrap
 """
 
 import tkinter as tk
-from tkinter import ttk
+#from tkinter import ttk
+import ttkbootstrap as ttk  # pip install ttkbootstrap
 
 TXT_TITLE = "Vägi"
 WIN_SIZE = "300x400"  # Default window size
 PAD_SIZE = 10 # GUI padding
 
+
 def cmd_teemidagi():
-  print('cmd_teemidagi')
-  print(entry_val.get())
+  input_var = entry_val.get()
+  output_string.set(f'Tehtud {input_var}')
 
 
-def main():
-  # Window
-  window = tk.Tk()
-  window.title(TXT_TITLE)
-  window.geometry(WIN_SIZE)
+# Window theme
+# 'journal' - light, red button
+# 'darkly' - dark, blue button
+window = ttk.Window(themename='darkly')
+window.title(TXT_TITLE)
+window.geometry(WIN_SIZE)  # Window start size
 
-  #Title
-  title_label = ttk.Label(master=window, text=TXT_TITLE, font='Futura 24')
-  title_label.pack()
+#Title
+title_label = ttk.Label(master=window, text=TXT_TITLE, font='Futura 24')
+title_label.pack()
 
-  # Input
-  input_frame = ttk.Frame(master=window)
-  global entry_val
-  entry_val = tk.IntVar()
-  entry = ttk.Entry(master=input_frame, textvariable=entry_val)
-  button = ttk.Button(master=input_frame, text='Tee midagi', command=cmd_teemidagi)
-  entry.pack(side='left', padx=PAD_SIZE)
-  button.pack(side='left')
-  input_frame.pack(pady=PAD_SIZE)
+# Input
+input_frame = ttk.Frame(master=window)
+entry_val = tk.IntVar()
+entry = ttk.Entry(master=input_frame, textvariable=entry_val)
+button = ttk.Button(master=input_frame, text='Tee midagi', command=cmd_teemidagi)
+entry.pack(side='left', padx=PAD_SIZE)
+button.pack(side='left')
+input_frame.pack(pady=PAD_SIZE)
 
-  # Output
-  output_label = ttk.Label(master=window, text='Output')
-  output_label.pack()
-
-
-  window.mainloop()
+# Output
+global output_string
+output_string = tk.StringVar()
+output_label = ttk.Label(master=window, text='Output', textvariable=output_string)
+output_label.pack(pady=PAD_SIZE)
 
 
-if __name__ == '__main__':
-  try:
-    main()
-  except KeyboardInterrupt:
-    # Exit when CTRL-C pressed
-    print('Closing!')
-    exit(0)
+window.mainloop()
